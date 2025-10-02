f180c2f960ec3cad0b66a42078ab73290c0e6dc6
Henry Williamson <henrygwilliamson@gmail.com>
2025-10-02 14:37:30 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KMPItemPage",
-            url: "https://github.com/HenryGW/kmp-item-page/releases/download/0.0.4/KMPItemPage-0.0.4.xcframework.zip",
+            url: "https://github.com/HenryGW/kmp-item-page/releases/download/0.0.5/KMPItemPage-0.0.5.xcframework.zip",
             checksum: "7382eb9a1c6cd24c475ed78a6b0e326a418de51b1bc1dad31cef3d96bca9c559"
         )
     ]
